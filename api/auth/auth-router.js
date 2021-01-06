497619c1cb9c78aa4577ee71c521446229b3e5de
will Fletcher <wfletch7@gmail.com>
2021-01-06 12:32:11 -0500

    testing for data stick

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -53,7 +53,10 @@ router.post('/login', checkPayload, (req, res) => {
         .then(([user]) => {
             if(user && bcrypt.compareSync(password, user.password)){
                 const token = createToken(user)
-                res.status(200).json(token)
+                res.status(200).json({
+                    message: 'welcome in, ' + user.username,
+                    token
+                })
             } else {
                 res.status(401).json("nice try, no token")
             }
