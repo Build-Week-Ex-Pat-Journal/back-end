e58122a67dd0efd259ebc3062725d437a1f410a7
will Fletcher <wfletch7@gmail.com>
2021-01-07 11:07:33 -0500

    added seeds to test if data is deleted or unavail

--- api/server.js
+++ api/server.js
@@ -4,12 +4,16 @@ const cors = require('cors')
 const server = express()
 const authRouter = require('./auth/auth-router')
 const postRouter = require('./posts/post-router')
+
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
+
 server.get("/", (req, res) => {
     res.json({api:"up"})
 })
+
 server.use('/api', authRouter)
 server.use('/posts', postRouter)
+
 module.exports = server
--- seeds/users.js
+++ seeds/users.js
@@ -1,13 +1,15 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('users').del()
+  return knex('users').truncate()
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 1, username: 'josh', password: '12345'},
-        {id: 2, username: 'mike', password: '12345'},
-        {id: 3, username: 'will', password: '12345'}
+        {id: 1, username: 'Josh', password: 'test12345'},
+        {id: 2, username: 'Mike', password: 'test12345'},
+        {id: 3, username: 'William', password: 'test12345'},
+        {id: 3, username: 'Marcos', password: 'test12345'},
+        {id: 3, username: 'Patrick', password: 'test12345'}
       ]);
     });
 };
